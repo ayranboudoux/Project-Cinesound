1244e04481817885a1daee29b71d89e66a506037
Ayran Boudoux <ayranboudoux.dev@gmail.com>
2026-05-21 09:13:29 -0300

    Criação da classe TituloItem

---
+++ app/src/main/java/com/example/cinesound/TituloItem.java
@@ -0,0 +1,16 @@
+package com.example.cinesound;
+
+import com.google.firebase.Timestamp;
+
+import java.util.List;
+
+public class TituloItem {
+    public int tmdbId;
+    public String titulo;
+    public String poster;
+    public String tipo;   //filme ou serie
+    public List<Integer> generos;
+    public double notaTmdb;
+    public String classificacao;   //preenchido so quando vem do firebase
+    public Timestamp dataClassificacao;
+}
