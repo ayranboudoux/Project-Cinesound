49f445b62929f5c3e91af9b8cfd8a1a7e424254b
Rayan Lima <rclima522@gmail.com>
2026-05-25 01:48:15 -0300

    feito isso e isso

---
+++ app/src/main/java/com/example/cinesound/DetalheTmdb.java
@@ -0,0 +1,34 @@
+package com.example.cinesound;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class DetalheTmdb {
+    public class DetalheTmdb {
+        @SerializedName("id")
+        public int id;
+        @SerializedName("title")
+        public String titulo;
+        @SerializedName("name")
+        public String nome;
+        @SerializedName("overview")
+        public String sinopse;
+        @SerializedName("poster_path")
+        public String posterPath;
+        @SerializedName("vote_average")
+        public double nota;
+        @SerializedName("runtime")
+        public int duracao;
+        @SerializedName("release_date")
+        public String dataLancamento;
+        @SerializedName("genres")
+        public List<GeneroPar> generos;
+        public static class GeneroPar {
+            @SerializedName("id")
+            public int id;
+            @SerializedName("name")
+            public String nome;
+        }
+    }
+}
